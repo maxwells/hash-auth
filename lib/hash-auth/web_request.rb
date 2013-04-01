fe303f235a6bbf56837c1e239dda8a09290fb979
Max Lahey <maxwellslahey@gmail.com>
2013-04-01 16:20:34 -0400

    refactored WebRequest to reduce code redundancy

--- lib/hash-auth/web_request.rb
+++ lib/hash-auth/web_request.rb
@@ -4,36 +4,41 @@ module HashAuth
   class WebRequest
 
     def self.get(client, url, headers = {}, &block)
-      headers = self.sign_and_identify client, :get, headers
-      RestClient.get url, headers, &block
+      self.delegate_to_rest_client_passive :get, client, url, headers, &block
     end
 
     def self.post(client, url, payload, headers = {}, &block)
-      payload = self.sign_and_identify client, :post, payload
-      RestClient.post url, payload, headers, &block
+      self.delegate_to_rest_client_active :post, client, url, payload, headers, &block
     end
 
     def self.patch(client, url, payload, headers = {}, &block)
-      payload = self.sign_and_identify client, :patch, payload
-      RestClient.patch url, payload, headers, &block
+      self.delegate_to_rest_client_active :post, client, url, payload, headers, &block
     end
 
     def self.put(client, url, payload, headers = {}, &block)
-      headers = self.sign_and_identify client, :put, headers
-      RestClient.put url, payload, headers, &block
+      self.delegate_to_rest_client_active :post, client, url, payload, headers, &block
     end
 
     def self.delete(client, url, headers = {}, &block)
-      headers = self.sign_and_identify client, :delete, headers
-      RestClient.delete url, headers, &block
+      self.delegate_to_rest_client_passive :delete, client, url, headers, &block
     end
 
     def self.head(client, url, headers = {}, &block)
-      headers = self.sign_and_identify client, :head, headers
+      self.delegate_to_rest_client_passive :head, client, url, headers, &block
     end
 
     def self.options(client, url, headers = {}, &block)
-      headers = self.sign_and_identify client, :options, headers
+      self.delegate_to_rest_client_passive :options, client, url, headers, &block
+    end
+
+    def self.delegate_to_rest_client_passive(action, client, url, headers, &block)
+      headers = self.sign_and_identify client, action, headers
+      RestClient.send action, url, headers, &block
+    end
+
+    def self.delegate_to_rest_client_active(action, client, url, payload, headers, &block)
+      payload = self.sign_and_identify client, action, payload
+      RestClient.send action, url, payload, headers, &block
     end
 
     def self.sign_and_identify(client, verb, params)
